55c100a7c2bda2b56fb608d96bbbedaa21f57972
Uzytkownik <uuzytkownik@kursGIT.pl>
2020-09-20 10:40:17 +0200

    Ex1

---
+++ src/sda/basic/java/day2/Ex1.java
@@ -0,0 +1,21 @@
+package sda.basic.java.day2;
+
+public class Ex1 {
+    public static void main(String[] args) {
+
+        System.out.println("Last char: " + getLastCharacterV1("domek"));
+        System.out.println("Last char: " + getLastCharacterV2("domek"));
+
+    }
+
+    private static char getLastCharacterV1(String input) {
+        int lastPosition = input.length() - 1;
+        return input.charAt(lastPosition);
+
+    }
+
+    private static String getLastCharacterV2(String input) {
+        int lastPosition = input.length() - 1;
+        return input.substring(lastPosition);
+    }
+}
