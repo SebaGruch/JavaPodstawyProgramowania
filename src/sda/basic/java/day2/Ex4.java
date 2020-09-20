d98453cf1f8bfa0f14f56f7bef767832b05c8b2c
Uzytkownik <uuzytkownik@kursGIT.pl>
2020-09-20 11:27:09 +0200

    Ex3

---
+++ src/sda/basic/java/day2/Ex4.java
@@ -0,0 +1,13 @@
+package sda.basic.java.day2;
+
+public class Ex4 {
+    public static void main(String[] args) {
+
+        replaceAndPrintString("Ala.ma.kota,");
+    }
+
+    private static void replaceAndPrintString(String input) {
+        String result = input.replace(",", "-STOP-").replace(".", "-STOP-");
+        System.out.println("new text " + result);
+    }
+}
