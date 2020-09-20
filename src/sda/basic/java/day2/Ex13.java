2d356cbd8b70afaa3fc9cf12d175fbcf7c48ccc4
Uzytkownik <uuzytkownik@kursGIT.pl>
2020-09-20 14:37:17 +0200

    Ex13

---
+++ src/sda/basic/java/day2/Ex13.java
@@ -0,0 +1,55 @@
+package sda.basic.java.day2;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Ex13 {
+
+    public static void main(String[] args) {
+        int[] arrays = {12, 7, 19};
+
+        int minValue = getMinValue(arrays);
+        System.out.println("Min value: " + minValue);
+
+        int maxValue = getMaxValue(arrays);
+        System.out.println("Max value: " + maxValue);
+
+        int sum = getSum(arrays);
+        System.out.println("Sum: " + sum);
+
+    }
+
+    private static int getMinValue(int[] arrays) {
+        int minValue = Integer.MAX_VALUE;
+        for (int i = 0; i < arrays.length; i++) {
+            if (minValue > arrays[i]) {
+                minValue = arrays[i];
+            }
+        }
+        return minValue;
+        // return Arrays.stream(arrays).min().getAsInt();
+    }
+
+    private static int getMaxValue(int[] arrays) {
+        int maxValue = Integer.MIN_VALUE;
+        for (int i = 0; i < arrays.length; i++) {
+            if (maxValue < arrays[i]) {
+                maxValue = arrays[i];
+            }
+        }
+        return maxValue;
+    }
+
+    private static int getSum(int[] arrays) {
+        int sum = 0;
+        for (int i = 0; i < arrays.length; i++) {
+            sum += arrays[i];
+        }
+
+        for (int i : arrays){
+            sum += i;
+        }
+        return sum;
+        // 7return Arrays.stream(arrays).sum();
+    }
+}
